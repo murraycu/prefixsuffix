2aa7b6a9375e432760492f18608c31a86e19c7bd
Murray Cumming <murrayc@murrayc.com>
2015-04-10 13:52:32 +0200

    Add the --enable-warnings configure/autogen.sh option.
    Because I like using --enable-warnings=fatal.
    And turn this on during distcheck.

--- autogen.sh
+++ autogen.sh
@@ -3,6 +3,7 @@ test -n "$srcdir" || srcdir=`dirname "$0"`
 test -n "$srcdir" || srcdir=.
 (
   cd "$srcdir" &&
+  mm-common-prepare --copy --force &&
   autopoint --force &&
   AUTOPOINT='intltoolize --automake --copy' autoreconf --force --install
 ) || exit
